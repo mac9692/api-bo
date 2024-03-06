b84aa522a017768ef631c27c81c69f7e16e55bde
박진성 <mac9692@nate.com>
2024-03-07 00:06:17 +0900

    feat: 상품 CR
    1. DTO 불변
    2. Entity 불변

--- src/main/java/com/js/apibo/goods/dto/response/GoodsDetailResponse.java
+++ src/main/java/com/js/apibo/goods/dto/response/GoodsDetailResponse.java
@@ -1,16 +1,16 @@
 package com.js.apibo.goods.dto.response;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Getter
-@Setter
 public class GoodsDetailResponse {
     private String goodsNo;
     private String goodsName;
     private String goodsDesc;
     private Timestamp sysRegDtm;
     private String sysRegId;
+
+    private GoodsDetailResponse() {}
 }
